b2bc7a8617081381216cc367001337619dd64414
JeremieAdams <42946942+JeremieAdams@users.noreply.github.com>
2018-09-07 08:38:51 -0400

    Change to Curl

--- auth.minionrevolution.com/callback.php
+++ auth.minionrevolution.com/callback.php
@@ -9,41 +9,31 @@
 	if(isset($_GET["code"])){
 		$codeReturn = ($_GET["code"]);
 		$url = 'https://login.eveonline.com/oauth/token HTTP/1.1';
-		$clientID = '5d842215afea43958e1b7e24ebef5d59';
-		$clientSecret = 'QigNtmznSL70QIMB75tL55msO8GcAoMEDuXFywLQ';
-		$baseEncodeClient = 'Basic ' . base64_encode($clientID . ":" . $clientSecret);
-		$data = http_build_query(array('grant_type' => 'authorization_code', 'code' => $codeReturn));
+		
+		$sql = "SELECT * FROM `auth_AppConfig` LIMIT 1";
+		$resultSql = $connection->query($sql);
+		$clientData = $resultSql->fetch_assoc();
+		$connection->close();
+		
+		$clientID = $clientData["auth_AppConfig_clientID"];
+		$clientSecret = $clientData["auth_AppConfig_clientSecret"];
+		$baseEncodeClient = 'Basic '.base64_encode($clientID.":".$clientSecret);
+		$headers = "Authorization: ".$baseEncodeClient."\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: login.eveonline.com";
+		$fields = http_build_query(array('grant_type' => 'authorization_code', 'code' => $codeReturn));
 
-		$options = array(
-			'http' => array(
-				'header'  => "Authorization: " . $baseEncodeClient . "\r\n
-								Content-Type: application/x-www-form-urlencoded\r\n
-								Host: login.eveonline.com",
-				'method'  => 'POST',
-				'content' => $data
-			)
-		);
-		$context  = stream_context_create($options);
-		$result = file_get_contents($url, false, $context);
-//		if ($result === FALSE) { }
+		$ch = curl_init();
 
-		var_dump($result);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, $headers);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		
+		$result = curl_exec($ch);
+
+		var_dump(json_decode($result));
 
 	} else {
 		$message = "Failure on submmission to EVE ESI SSO";
 	}
 ?>
-
-<html>
-	<br />
-	<?php echo $codeReturn ?><br />
-	
-	<?php echo $baseEncodeClient ?><br />
-	
-	<?php foreach($options['http'] as $result) {
-		echo $result, '<br />';
-	} ?><br />
-	
-	<?php echo $data ?><br />
-
-</html>
