e814a92e618a9bd8ac93d6b2595c3f6e76e7acda
JeremieAdams <42946942+JeremieAdams@users.noreply.github.com>
2018-09-07 23:20:24 -0400

    Major refactor of CURL request

--- auth.minionrevolution.com/callback.php
+++ auth.minionrevolution.com/callback.php
@@ -8,7 +8,7 @@
 	
 	if(isset($_GET["code"])){
 		$codeReturn = ($_GET["code"]);
-		$url = 'https://login.eveonline.com/oauth/token HTTP/1.1';
+		$url = 'https://login.eveonline.com/oauth/token';
 		
 		$sql = "SELECT * FROM `auth_AppConfig` LIMIT 1";
 		$resultSql = $connection->query($sql);
@@ -17,22 +17,32 @@
 		
 		$clientID = $clientData["auth_AppConfig_clientID"];
 		$clientSecret = $clientData["auth_AppConfig_clientSecret"];
-		$baseEncodeClient = 'Basic '.base64_encode($clientID.":".$clientSecret);
-		$headers = "Authorization: ".$baseEncodeClient."\r\nContent-Type: application/x-www-form-urlencoded\r\nHost: login.eveonline.com";
+		$baseEncodeClient = base64_encode($clientID.":".$clientSecret);
+		$headers = "authorization: ".$baseEncodeClient."\r\ncontent-type: application/x-www-form-urlencoded\r\nhost: login.eveonline.com";
 		$fields = http_build_query(array('grant_type' => 'authorization_code', 'code' => $codeReturn));
 
 		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, $headers);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		
-		$result = curl_exec($ch);
+		curl_setopt_array($ch, array(
+		  CURLOPT_URL => "https://login.eveonline.com/oauth/token",
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => "grant_type=authorization_code&code=".$codeReturn,
+		  CURLOPT_HTTPHEADER => array(
+			"authorization: Basic " . base64_encode($clientId . ":" . $clientSecret),
+			"content-type: application/x-www-form-urlencoded"
+		  ),
+		));
 
-		var_dump(json_decode($result));
+		$response = curl_exec($ch);
+		$err = curl_error($ch);
 
+		var_dump($response);
+		
+		echo "<br /><br />";
+		
+		var_dump($ch);
+
+		
 	} else {
 		$message = "Failure on submmission to EVE ESI SSO";
 	}
